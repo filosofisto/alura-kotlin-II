384b128644111c4e66931930678123f0ec34b849
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2019-03-11 08:35:38 -0300

    Init

---
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,66 @@
+package br.com.alura.financask.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.ViewGroup
+import br.com.alura.financask.R
+import br.com.alura.financask.delegate.TransacaoDelegate
+import br.com.alura.financask.model.Tipo
+import br.com.alura.financask.model.Transacao
+import br.com.alura.financask.ui.ResumoView
+import br.com.alura.financask.ui.adapter.ListaTransacoesAdapter
+import br.com.alura.financask.ui.dialog.AdicionaTransacaoDialog
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    private val transacoes: MutableList<Transacao> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+
+        configuraResumo()
+        configuraLista()
+        configuraFab()
+    }
+
+    private fun configuraFab() {
+        lista_transacoes_adiciona_receita
+                .setOnClickListener {
+                    chamaDialogDeAdicao(Tipo.RECEITA)
+                }
+        lista_transacoes_adiciona_despesa
+                .setOnClickListener {
+                    chamaDialogDeAdicao(Tipo.DESPESA)
+                }
+    }
+
+    private fun chamaDialogDeAdicao(tipo: Tipo) {
+        AdicionaTransacaoDialog(window.decorView as ViewGroup, this)
+                .chama(tipo, object : TransacaoDelegate {
+                    override fun delegate(transacao: Transacao) {
+                        atualizaTransacoes(transacao)
+                        lista_transacoes_adiciona_menu.close(true)
+                    }
+                })
+    }
+
+    private fun atualizaTransacoes(transacao: Transacao) {
+        transacoes.add(transacao)
+        configuraLista()
+        configuraResumo()
+    }
+
+    private fun configuraResumo() {
+        val view: View = window.decorView
+        val resumoView = ResumoView(this, view, transacoes)
+        resumoView.atualiza()
+    }
+
+    private fun configuraLista() {
+        lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
+    }
+
+}
